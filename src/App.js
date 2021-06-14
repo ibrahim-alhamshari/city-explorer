0e422b9e494e3ab045a0a49b4b43a992514ab040
ibrahim-alhamshari <ibrahimalhamshari742@gmail.com>
2021-06-14 14:32:32 +0300

    making try ... catch way

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import ErrorBoundary from './Error';
-// import Image from 'react-bootstrap/Image';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Alert from 'react-bootstrap/Alert';
+import Image from 'react-bootstrap/Image';
+
 
 
 export class App extends Component {
@@ -10,7 +12,10 @@ export class App extends Component {
     this.state = {
       cityName: '',
       cityData: {},
-      displayData: false
+      displayData: false,
+      error: '',
+      message: 'please',
+      alert: false,
     }
   }
 
@@ -23,18 +28,30 @@ export class App extends Component {
 
   getAPIData = async (e) => {
     e.preventDefault();
+    try{
     const axiosResponse = await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.00d7cd872eadb75fc70c28a8fb0a50b5&q=${this.state.cityName}&format=json`)
 
     this.setState({
       cityData: axiosResponse.data[0],
       displayData: true,
     })
+  }catch(error){
+this.setState({
+  error: error.message,
+  alert : true
+})
+  }
   }
 
   render() {
     return (
       <div>
-        <ErrorBoundary>
+        {this.state.alert &&
+        <Alert variant="danger">
+        This is a {this.state.error} alert-check it out!
+        </Alert>
+
+        }
         <h2>City Explorer</h2>
         <form onSubmit={this.getAPIData}>
           <label>City name</label>
@@ -47,10 +64,10 @@ export class App extends Component {
         {this.state.displayData &&
         <div>
           <p>{this.state.cityData.display_name}</p>
-          <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.d36871f015649f915282f374cff76628&q&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=15`} alt='' />
+          {/* <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.d36871f015649f915282f374cff76628&q&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=15`} alt='' /> */}
+          <Image src={`https://maps.locationiq.com/v3/staticmap?key=pk.d36871f015649f915282f374cff76628&q&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=15`} alt='' />
         </div>
         }
-        </ErrorBoundary>
       </div>
       
     )
